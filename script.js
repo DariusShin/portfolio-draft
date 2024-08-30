92b788829ef8ae2cdad6b84d3d5ca6310aa58e27
Darius Lee <dariuslee0614@gmail.com>
2024-08-30 23:52:52 +0800

    Completed Header section.

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ const DOMS = {
     body: document.body,
     toggleThemeButton: document.querySelector('.button-toggle'),
     backToTopButton: document.getElementById('back-to-top'),
+    scrollLinks: document.querySelectorAll('.scroll-link'),
 };
 
 // Handler to manually toggle color theme
 DOMS.toggleThemeButton.addEventListener('click', () => {
     DOMS.body.classList.toggle("dark");
+});
+
+// Handler to toggle state of anchor elements
+DOMS.scrollLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        // Remove 'active-link' class from all links
+        document.querySelectorAll('.scroll-link').forEach(item => {
+            item.classList.remove('active-link');
+        });
+        // Add 'active-link' class to the clicked link
+        this.classList.add('active-link');
+    });
 });
 
 // Back to Top button functionality
@@ -22,6 +35,7 @@ function scrollFunction() {
 }
 
 DOMS.backToTopButton.onclick = function() {
-    DOMS.body.scrollTop = 0; // For Safari
+    DOMS.body.scrollTop = 0; // For Safari browser
     document.documentElement.scrollTop = 0;
 }
+
